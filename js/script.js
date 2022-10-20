6f44bee1060e2746debee605b84f6fb044ef129f
jgn35 <115986615+jgn35@users.noreply.github.com>
2022-10-20 11:34:13 +0200

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":900},"pages":[{"id":"page-vote-page","displayName":"Vote-page","link":{"linkType":"LinkTypePage","href":"#!page-vote-page"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-loginpage","displayName":"loginPage","link":{"linkType":"LinkTypePage","href":"#!page-loginpage"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
