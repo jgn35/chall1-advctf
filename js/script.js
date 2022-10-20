da60e5bc44827574046914e21c5dc67a6077be89
jgn35 <115986615+jgn35@users.noreply.github.com>
2022-10-20 13:36:02 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":900},"pages":[{"id":"page-vote-page","displayName":"Vote-page","link":{"linkType":"LinkTypePage","href":"#!page-vote-page"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-loginpage","displayName":"loginPage","link":{"linkType":"LinkTypePage","href":"#!page-loginpage"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+    window.silex.data = {"site":{"width":900},"pages":[{"id":"page-index","displayName":"index","link":{"linkType":"LinkTypePage","href":"#!page-index"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-vote","displayName":"vote","link":{"linkType":"LinkTypePage","href":"#!page-vote"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
